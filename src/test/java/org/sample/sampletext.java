9a1a1fd288ce2d16046b5eec03df7a477fccc5c6
divyavenkat28 <divyavenkat28@gmail.com>
2022-01-29 11:31:48 +0530

    Divya's updated code

--- src/test/java/org/sample/sampletext.java
+++ src/test/java/org/sample/sampletext.java
@@ -4,5 +4,8 @@ public class sampletext {
 public static void main(String[] args) {
 	System.out.println("This is for git practice");
 	System.out.println("new text");
+	int a=10,b=20;
+	int c=a+b;
+	System.out.println("The value for c:"+c);
 }
 }
